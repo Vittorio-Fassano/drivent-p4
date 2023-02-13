d65ecec88493f9ff8a698f2e8bcdafdf06a25dbb
Vittorio <vittfassano@gmail.com>
2023-02-13 13:24:33 -0300

    feat: putBooking tests

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -3,7 +3,6 @@ import { forbiddenError } from "@/errors/forbiden-error";
 import bookingRepository from "@/repositories/booking-repository";
 import enrollmentRepository from "@/repositories/enrollment-repository";
 import roomRepository from "@/repositories/room-repository";
-import ticketRepository from "@/repositories/ticket-repository";
 
 async function getBookingService(userId: number) {
   const booking = await bookingRepository.findBookingById(userId);
--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -11,7 +11,6 @@ import {
   createPayment,
   createRoomWithHotelId,
   createTicket,
-  createTicketTypeRemote,
   createTicketTypeWithHotel,
   createTicketTypeWithoutHotel,
   createUser,
@@ -176,6 +175,117 @@ describe("POST /booking", () => {
       const createdRoom = await createRoomWithHotelId(createdHotel.id);
 
       const response = await server.post("/booking").set("Authorization", "Bearer "+token).send({
+        roomId: createdRoom.id,
+      });
+      async function findBookingFirst() {
+        return prisma.booking.findFirst({
+          orderBy: {
+            id: "desc",
+          },
+        });
+      }
+      const find = await findBookingFirst();
+      expect(response.status).toBe(httpStatus.OK);
+      expect(response.body).toEqual({ bookingId: find.id });
+    });
+  });
+});
+
+describe("PUT /booking/:bookingId", () => {
+  it("should respond with status 401 if no token is given", async () => {
+    const response = await server.put("/booking/1");
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.put("/booking/1").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.put("/booking/1").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 404 if enrollment does not exist for this user", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createTicketTypeWithoutHotel();
+      const createdHotel = await createHotel();
+      const createdRoom = await createRoomWithHotelId(createdHotel.id);
+      await createBooking(user.id, createdRoom.id);
+
+      const response = await server.put("/booking/1").set("Authorization", "Bearer "+token).send({
+        roomId: createdRoom.id,
+      });
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });
+
+    it("should respond with status 404 if roomId does not exist", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const createdHotel = await createHotel();
+      const createdRoom = await createRoomWithHotelId(createdHotel.id);
+      await createBooking(user.id, createdRoom.id);
+
+      const response = await server.put("/booking/1").set("Authorization", "Bearer "+token).send({
+        roomId: 0,
+      });
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });
+
+    it("should respond with status 403 if user does not have booking", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const createdHotel = await createHotel();
+      const createdRoom = await createRoomWithHotelId(createdHotel.id);
+
+      const response = await server.put("/booking/1").set("Authorization", "Bearer "+token).send({
+        roomId: createdRoom.id,
+      });
+      expect(response.status).toBe(httpStatus.FORBIDDEN);
+    });
+
+    it("should respond with status 403 if roomId does not have space", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithoutHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      await createPayment(ticket.id, ticketType.price);
+      const createdHotel = await createHotel();
+      const createdRoom = await createRoomWithHotelId(createdHotel.id, 1);
+      await createBooking(user.id, createdRoom.id);
+
+      const response = await server.put("/booking/1").set("Authorization", "Bearer "+token).send({
+        roomId: createdRoom.id,
+      });
+      expect(response.status).toBe(httpStatus.FORBIDDEN);
+    });
+
+    it("should respond with status 200 and bookingId", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      await createPayment(ticket.id, ticketType.price);
+      const createdHotel = await createHotel();
+      const createdRoom = await createRoomWithHotelId(createdHotel.id);
+      const createdBooking = await createBooking(user.id, createdRoom.id);
+      const response = await server.put(`/booking/${createdBooking.id}`).set("Authorization", `Bearer ${token}`).send({
         roomId: createdRoom.id,
       });
       async function findBookingFirst() {
